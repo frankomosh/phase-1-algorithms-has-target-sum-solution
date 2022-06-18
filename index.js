19aa5e7ff513f80a84f329a973e395779607b2ab
Frank-197 <frankomosh197@gmail.com>
2022-06-18 22:25:18 +0300

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i=0;i<array.length;i++){
+    const complement=target-array[i];
+    for (let j=i+1;j<array.length;j++){
+      if(array[j]===complement) return true;
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  0(n*n)
 */
 
 /* 
   Add your pseudocode here
+  iterate over array of numbers
+    for the current number, identify complimentary number which can add to target
+    iterate over remaining array numbers
+   check to see which one in the remaining bunch is a complement
+    if found, return false
+   if end of array is reached and complement not found, return false  
 */
 
 /*
   Add written explanation of your solution here
+  The function takes in two parameters, the array and a target. A for loop is defined to iterate over each elements in the array.
+  A constant variable known as compliment seeks to determine the value that can be added to an element in an array to find the target.
+  The function then iterates over the remaining values in array to determine the availability of the complement.
+  If found, the functio would return true and if not, would return a false.
 */
 
 // You can run `node index.js` to view these console logs
